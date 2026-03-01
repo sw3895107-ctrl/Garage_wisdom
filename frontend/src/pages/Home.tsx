b05d7b618f27559ff8e0f7209f143f91c5411982
sw3895107-ctrl <swolfe2323@gmail.com>
2026-03-01 03:11:36 -0500

    Create Home.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Welcome to Garage Wisdom</h1>
+    <p>Browse fixes and save money on car repairs!</p>
+  </div>
+);
+
+export default Home;
